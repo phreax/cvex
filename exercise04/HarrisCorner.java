cfa5b7775bed0a0eaed4121e31c0bfc9bc5a96eb
Michael Thomas <m.thomas@networksec.de>
2011-05-13 21:02:17 +0200

    test & demo

--- exercise04/HarrisCorner.java
+++ exercise04/HarrisCorner.java
@@ -5,7 +5,7 @@
  * with aproximated Gaussian second order derivatives and
  * integral images
  *
- * @version 0.1  2011-04-29
+ * @version 0.1  2011-05-13
  * @author Michael Thomas, Jan Swoboda
  */
 
@@ -14,13 +14,14 @@ import Jama.Matrix;
 import graphic.*;
 import java.lang.Math;
 import java.lang.IllegalArgumentException;
+import java.lang.ArrayIndexOutOfBoundsException;
+
 
 public class HarrisCorner {
 
-    /** find corners
-     *
-     * @return -- local maxima of cornerness
-     */
+
+    // maxima of the blob response
+    public int[][] blobmaxima;
 
     private static int roundToUneven(double d) {
         int r = (int)Math.floor(d);
@@ -44,12 +45,17 @@ public class HarrisCorner {
             case GRADIENT_XX:
                int subcols = roundToUneven(0.6*size);
                int subrows = size/3;
-               int j0 = (size-subrows)/2;
-               int j1 = j0+subrows-1;
+               //System.out.printf("submatrix: (%d,%d)\n", subcols,subrows);
+               int j0 = (size-subcols)/2;
+               int j1 = j0+subcols-1;
 
-               subwhite = new Matrix(subcols,subrows,1.0);
-               subblack = new Matrix(subcols,subrows,-2.0);
+               System.out.printf("j0,j1: (%d,%d)\n", j0,j1);
+               subwhite = new Matrix(subrows,subcols,1.0);
+               subblack = new Matrix(subrows,subcols,-2.0);
 
+               //System.out.println("Submatrices:");
+               subwhite.print(1,0);
+                   
                kernel.setMatrix(0,subrows-1,j0,j1,subwhite);
                kernel.setMatrix(subrows,(2*subrows)-1,j0,j1,subblack);
                kernel.setMatrix(2*subrows,size-1,j0,j1,subwhite);
@@ -59,7 +65,9 @@ public class HarrisCorner {
                   break;
             case GRADIENT_XY:
                int subdim = size/3;
-               int padding = (2*subdim-1)/2; // distance from border
+               int padding = (subdim-1)/2; // distance from border
+               
+               //System.out.printf("dim,padding: (%d,%d)\n", subdim,padding);
 
                subwhite = new Matrix(subdim,subdim,1.0);
                subblack = new Matrix(subdim,subdim,-2.0);
@@ -73,26 +81,109 @@ public class HarrisCorner {
 
         return kernel;
     }
-               
-    public short[][] findCorners(Image image) {
+    
+    /** sum up pixel intensities over
+     *  a reagion, using integral images
+     *
+     *  @param image  -- integral image
+     *  @param region -- rectangle defining the region
+     *  @return       -- sum_{(x,y) in region)} I(x,y)
+     **/
+    private static int sumIntensities(int[][] image, Neighborhood region) {
 
-        int windowsize = 9;
+        int w = image.length;
+        int h = image[0].length;
+
+        // corners of rectangular region 
+        Point a,b,c,d;
+        a = region.point(0,0);
+        c = region.point(region.getSize()-1,region.getSize()-1);
+        b = new Point(c.x,a.y);
+        d = new Point(a.x,c.y);
+
+        // check boundaries
+        if(a.getX() <0) a.x = 0;
+        if(a.getY() <0) a.y = 0;
+        if(b.getX() >= w) b.x = w-1;
+        if(b.getY() <0) b.y = 0;
+        if(c.getX() >= w) c.x = w-1;
+        if(c.getY() >= h) c.y = h-1;
+        if(d.getX() <0) d.x = 0;
+        if(d.getY() >=h) d.y = h-1;
+
+        int val;
+        try {
+            val = image[a.getX()][a.getY()] + image[c.getX()][c.getY()] - image[b.getX()][b.getY()]
+                                                                    - image[d.getX()][d.getY()];
+        } catch(ArrayIndexOutOfBoundsException e) {
+            System.out.printf("Image dimensions: %dx%d\n", w,h );
+            System.out.printf("a = %s\n", a);
+            System.out.printf("b = %s\n", b);
+            System.out.printf("c = %s\n", c);
+            System.out.printf("d = %s\n", d);
+            throw(e);
+        }
+            
+            
+
+        return val;
+     
+    }
+    
+
+    public void findCorners(Image image, int windowsize) {
 
         int w = image.width();
         int h = image.height();
 
         // derivative images
-        float[][] imgDxx; 
-        float[][] imgDxy; 
-        float[][] imgDyy; 
+        short[][] imgDxx; 
+        short[][] imgDxy; 
+        short[][] imgDyy; 
 
         // integral images of the derivatives
-        float[][] imgIntDxx; 
-        float[][] imgIntDxy; 
-        float[][] imgIntDyy; 
+        int[][] imgIntDxx; 
+        int[][] imgIntDxy; 
+        int[][] imgIntDyy; 
 
+        // blob response map
+        int[][] blobresponses = new int[w][h];
 
-        return null;
+        // determinante of hessian, refers
+        // to blobresponse
+        int det;
+
+        // sum of gradients in point over the window 
+        int sumDxx, sumDxy, sumDyy;
+
+        // compute gradients
+        imgDxx = Convolution.filter(image,gradientKernel(GRADIENT_XX,windowsize));
+        imgDxy = Convolution.filter(image,gradientKernel(GRADIENT_XY,windowsize));
+        imgDyy = Convolution.filter(image,gradientKernel(GRADIENT_YY,windowsize));
+
+        // compute integral images of gradients
+        imgIntDxx = Integral.compute(imgDxx);
+        imgIntDxy = Integral.compute(imgDxy);
+        imgIntDyy = Integral.compute(imgDyy);
+
+        // window
+        Neighborhood window;
+
+        for(int i=0;i<w;i++) 
+            for(int j=0;j<h;j++) {
+    
+                window = new Neighborhood(new Point(i,j),3);
+
+                sumDxx = sumIntensities(imgIntDxx,window);
+                sumDxy = sumIntensities(imgIntDxy,window);
+                sumDyy = sumIntensities(imgIntDyy,window);
+
+                // compute determinante of hessian
+                det = (int) (sumDxx * sumDyy - (0.9*sumDxy*0.9*sumDxy));
+                blobresponses[i][j] = det;
+            }
+
+        this.blobmaxima = MinMax.max(blobresponses,3);
     }
 }
 
---
+++ exercise04/HarrisCornerTest.java
@@ -0,0 +1,67 @@
+/**
+ * HarrisCornerTest.java
+ * 
+ * unit tests for harris corner functions
+ *
+ * @version 0.1  2011-05-13
+ * @author Michael Thomas, Jan Swoboda
+ */
+package exercise04;
+import  graphic.*;
+import  exercise04.*;
+import  Jama.Matrix;
+
+public class HarrisCornerTest {
+
+    public static void testGradientKernel() {
+
+        Matrix k9xx = HarrisCorner.gradientKernel(0,9);
+        System.out.println("9x9 Kernel in XX:");
+        k9xx.print(1,0);
+        
+        Matrix k9xy = HarrisCorner.gradientKernel(2,9);
+        System.out.println("9x9 Kernel in XY:");
+        k9xy.print(1,0);
+
+        Matrix k9yy = HarrisCorner.gradientKernel(1,9);
+        System.out.println("9x9 Kernel in YY:");
+        k9yy.print(1,0);
+
+        Matrix k15xx = HarrisCorner.gradientKernel(0,15);
+        System.out.println("15x15 Kernel in XX:");
+        k15xx.print(1,0);
+        
+        Matrix k15xy = HarrisCorner.gradientKernel(2,15);
+        System.out.println("15x15 Kernel in XY:");
+        k15xy.print(1,0);
+
+        Matrix k15yy = HarrisCorner.gradientKernel(1,15);
+        System.out.println("15x15 Kernel in YY:");
+        k15yy.print(1,0);
+
+    }
+
+    public static void testBlobMaxima(Image image) {
+
+        HarrisCorner hc = new HarrisCorner();
+
+        hc.findCorners(image,9);
+
+        Painter painterMaxima = new Painter("Blob Maxima",new Image(hc.blobmaxima));
+       
+        for(int i=0;i<image.width();i++)
+            for(int j=0;j<image.height();j++) 
+                if(hc.blobmaxima[i][j] > 0) 
+                    image.setPixel(i,j,new Color(255,0,0)); 
+        Painter painterOriginal = new Painter("original",image);
+    }
+
+    public static void main (String [] args)
+    {
+        Image original = new Image(args[0]);
+        
+        testGradientKernel();
+        testBlobMaxima(original);
+    }
+}
+            
